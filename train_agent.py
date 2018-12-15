f011bab29fa2cdbb7d194a576f8dd82b959b2aa1
bakoe <ba.koe96@gmail.com>
2018-12-15 23:49:55 +0100

    Deactivate TensorBoard logging by default (#6)

--- train_agent.py
+++ train_agent.py
@@ -15,7 +15,7 @@ import time
 @click.option("--gpu/--no-gpu", default=False)
 @click.option("--imagefile", "-i", default='image_locations.txt', help="Path to the file containing the image locations.", type=click.Path(exists=True))
 @click.option("--boxfile", "-b", default='bounding_boxes.npy', help="Path to the bounding boxes.", type=click.Path(exists=True))
-@click.option("--tensorboard/--no-tensorboard", default=True)
+@click.option("--tensorboard/--no-tensorboard", default=False)
 def main(steps, gpu, imagefile, boxfile, tensorboard):
     print(steps)
     print(gpu)
