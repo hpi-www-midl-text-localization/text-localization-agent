794d1c0b4675718e52b899da0217c1b37a0b9d8f
Bastian König <ba.koe96@gmail.com>
2018-12-16 00:15:37 +0100

    Allow logging to TensorBoard (#6) (#8)
    * Allow logging to TensorBoard using tb_chainer (#6)
    
    * Add TensorBoard logging of average_q (#6)
    
    * Add TensorBoard logging of average_loss (#6)
    
    * Add TensorBoard logging of environment image (#6)
    
    * Use explicit font for TensorBoard rendering (#6)
    
    * Deactivate TensorBoard logging by default (#6)
    
    * Add instructions for TensorBoard to README.md (#6)

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # text-localization-agent
+
 The code to train the agent.
+
+## Prequisites
+
+You need Python 3 (preferably 3.6) installed, as well as the requirements from `requirements.txt`:
+
+```bash
+$ pip install -r requirements.txt 
+```
+
+Furthermore, you need to install the [text-localization-environment](https://github.com/hpi-www-midl-text-localization/text-localization-environment) by following its **Installation** instructions.
+
+## TensorBoard
+
+If you would like the program to generate log-files appropriate for visualization in TensorBoard, you need to:
+
+* Install **tensorflow**
+  ```bash
+  $ pip install tensorflow
+  ```
+  (If you use Python 3.7 and the installation fails, use: `pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.12.0-py3-none-any.whl
+` instead. [See here, why.](https://github.com/tensorflow/tensorflow/issues/20444#issuecomment-442767411))
+* Run the *text-localization-agent* program with the `--tensorboard` flag
+   ```bash
+   $ python train-agent.py --tensorboard --imagefile … --boxfile …
+   ``` 
+* Start TensorBoard pointing to the `tensorboard/` directory inside the *text-localization-agent* project
+   ```bash
+   $ tensorboard --logdir=<path to text-localization-agent>/tensorboard/
+   …
+   TensorBoard 1.12.0 at <link to TensorBoard UI> (Press CTRL+C to quit)
+   ``` 
+* Open the TensorBoard UI via the link that is provided when the `tensorboard` program is started (usually: http://localhost:6006)
--- train_agent.py
+++ train_agent.py
@@ -1,20 +1,22 @@
 import click
 import os
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 from text_localization_environment import TextLocEnv
 import chainer
 import chainerrl
 import logging
 import sys
-
+from tb_chainer import SummaryWriter
+import time
 
 @click.command()
 @click.option("--steps", "-s", default=2000, help="Amount of steps to train the agent.")
 @click.option("--gpu/--no-gpu", default=False)
 @click.option("--imagefile", "-i", default='image_locations.txt', help="Path to the file containing the image locations.", type=click.Path(exists=True))
 @click.option("--boxfile", "-b", default='bounding_boxes.npy', help="Path to the bounding boxes.", type=click.Path(exists=True))
-def main(steps, gpu, imagefile, boxfile):
+@click.option("--tensorboard/--no-tensorboard", default=False)
+def main(steps, gpu, imagefile, boxfile, tensorboard):
     print(steps)
     print(gpu)
     print(imagefile)
@@ -63,16 +65,66 @@ def main(steps, gpu, imagefile, boxfile):
 
     logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='')
 
+    step_hooks = []
+    if tensorboard:
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        agentClassName = agent.__class__.__name__[:10]
+        writer = SummaryWriter("tensorboard/tensorBoard_exp_" + timestr + "_" + agentClassName)
+        step_hooks = [TensorBoardLoggingStepHook(writer)]
+
     chainerrl.experiments.train_agent_with_evaluation(
         agent, env,
         steps=steps,  # Train the agent for 5000 steps
         eval_n_runs=10,  # 10 episodes are sampled for each evaluation
         max_episode_len=50,  # Maximum length of each episodes
         eval_interval=500,  # Evaluate the agent after every 100 steps
-        outdir='result')  # Save everything to 'result' directory
+        outdir='result', # Save everything to 'result' directory
+        step_hooks=step_hooks)
 
     agent.save('agent')
 
+class TensorBoardLoggingStepHook(chainerrl.experiments.StepHook):
+    def __init__(self, summary_writer):
+        self.summary_writer = summary_writer
+        return
+
+    def __call__(self, env, agent, step):
+        step_count = agent.t
+
+        self.summary_writer.add_scalar('average_q', agent.average_q, step_count)
+
+        self.summary_writer.add_scalar('average_loss', agent.average_loss, step_count)
+
+        last_action_name = env.action_set[agent.last_action].__name__
+        debug_image = self.get_debug_image_from_environment_image(image = env.episode_image.copy(),
+                                                                  bbox = env.bbox,
+                                                                  last_action_name = last_action_name)
+
+        # Prepare the image for further use in rb_chainer's add_image() method
+        debug_image = np.array(debug_image, np.float32) / 255
+        debug_image = debug_image.transpose((2, 0, 1))
+
+        self.summary_writer.add_image('episode_image_debug', debug_image, step_count)
+        return
+
+    def get_debug_image_from_environment_image(self, image, bbox, last_action_name):
+        image = image.copy()
+
+        debug_image = Image.new(mode='RGB',
+                                size=(image.width, image.height + 16),
+                                color='black')
+        debug_image.paste(image)
+
+        draw = ImageDraw.Draw(debug_image)
+        draw.rectangle(bbox.tolist(), outline='white')
+        draw.rectangle((0, image.height, image.width, image.height + 16), fill='black')
+        font = ImageFont.truetype('fonts/open-sans/OpenSans-SemiBold.ttf', 12)
+        draw.text(xy=(2, image.height),
+                  text='Last action: %s' % last_action_name,
+                  fill='white',
+                  font=font)
+
+        return debug_image
 
 if __name__ == '__main__':
     main()
